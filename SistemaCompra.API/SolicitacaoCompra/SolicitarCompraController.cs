aae311ef981fcc236df3844040f3af87cbdc47e6
Felipe Souza <felipe6ouza@outlook.com>
2023-05-01 20:58:10 -0300

    Adiciona SolicitarCompraController

---
+++ SistemaCompra.API/SolicitacaoCompra/SolicitarCompraController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SistemaCompra.Application.SolicitacaoCompra.Command.RegistrarCompra;
+
+namespace SistemaCompra.API.SolicitacaoCompra
+{
+    public class SolicitarCompraController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SolicitarCompraController(IMediator mediator) => _mediator = mediator;
+
+        [HttpPost, Route("compra/solicitar")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public IActionResult SolicitarCompra([FromBody] RegistrarCompraCommand registrarCompraCommand)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+
+            _mediator.Send(registrarCompraCommand);
+            return Accepted();
+        }
+
+    }
+}
